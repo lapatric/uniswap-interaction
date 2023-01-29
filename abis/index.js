7e71903d1026224cfa44e5e345d531114892e5e5
lapatric <42653152+lapatric@users.noreply.github.com>
2023-01-29 18:33:38 +0100

    test uniswap.swapExactETHForTokens()

---
+++ abis/index.js
@@ -0,0 +1,5 @@
+const uniswapv2router02_abi = require("./uniswapv2router02.json");
+
+module.exports = {
+    uniswapv2router02_abi,
+}
--- scripts/uniswap_interaction.js
+++ scripts/uniswap_interaction.js
@@ -1,7 +1,10 @@
-const { ChainId, Fetcher, WETH, Route, Trade, TokenAmount, TradeType} = require('@uniswap/sdk')
+const { ChainId, Fetcher, WETH, Route, Trade, TokenAmount, TradeType, Percent} = require('@uniswap/sdk');
+const { ethers } = require("hardhat");
+const { uniswapv2router02_abi } = require("../abis");
 
 const chainId = ChainId.MAINNET;
 const tokenAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+const uniswapRouterAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
 
 async function init() {
     const dai = await Fetcher.fetchTokenData(chainId, tokenAddress);
@@ -13,6 +16,31 @@ async function init() {
     // console.log(route.midPrice.invert().toSignificant(6));
     console.log(trade.executionPrice.toSignificant(6));
     console.log(trade.nextMidPrice.toSignificant(6));
+
+    const accounts = await ethers.getSigners();
+    const ethBalance = await ethers.provider.getBalance(accounts[0].address);
+    console.log(ethers.utils.formatEther(ethBalance));
+
+    const slippageTolerance = new Percent('50', '10000'); // 50 bips. 1 bip = 0.0001 or 0.1 permille
+    const amountOutMin = ethers.BigNumber.from(trade.minimumAmountOut(slippageTolerance).raw.toString());
+    const path = [weth.address, dai.address];
+    const to = accounts[0].address;
+    const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20min from now (time in seconds)
+    const value = ethers.BigNumber.from(trade.inputAmount.raw.toString());
+    console.log(amountOutMin, path, to, deadline, value);
+
+    // const deployer = (await getNamedAccounts()).deployer;
+    const uniswap = new ethers.Contract(uniswapRouterAddress, uniswapv2router02_abi, accounts[0]);
+    const tx = await uniswap.swapExactETHForTokens(
+        amountOutMin,
+        path,
+        to,
+        deadline,
+        { value: value, gasPrice: 20e9, gasLimit: 250000 }
+    );
+    const receipt = await tx.wait(1)
+    console.log(`Transaction hash: ${tx.hash}`);
+    console.log(`Transaction was mined in block ${receipt.blockNumber}`);
 };
 
 init();
